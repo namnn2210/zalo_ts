1c52536696a39a7c339e5f20248340834828a5de
namngocngo22 <nathan.ng@sparkgo.ai>
2025-09-13 19:35:24 +0700

    add new account

---
+++ src/accounts/tlybinh.ts
@@ -0,0 +1,26 @@
+import { AccountConfig } from "../types";
+
+const tlybinh: AccountConfig = {
+    name: "T Lý Bình",
+    cookieFile: "tlybinh.json",
+    imei: "b5be8cb7-770a-4a9b-aec2-79fa821f79d1-3ade46e10ab46df1d7d395ddaa715a24",
+    ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+    targetList: {
+        '5263261561822696371': {
+            // 'from': '1879787845639791151',
+            'from': '3866146296658241899',
+            'to': ['5057805123328337545', 
+                '3538055945044932768', 
+                '5592164454266291746', 
+                '6737607174814223673', 
+                '8002750820125831350', 
+                '7809237214150566886', 
+                '78704608260425390', 
+                '3134274995891061958', 
+                '7802884328958834160'],
+        }
+    }
+};
+
+export default tlybinh;
+
--- src/config/accounts.ts
+++ src/config/accounts.ts
@@ -1,8 +1,8 @@
 import nguyenhai from "../accounts/nguyenhai.js";
-import namngo from "../accounts/namngo.js";
+import tlybinh from "../accounts/tlybinh.js";
 import { AccountConfig } from "../types";
 import dongbinh from "../accounts/dongbinh.js";
 
-const accounts: AccountConfig[] = [nguyenhai, dongbinh]
+const accounts: AccountConfig[] = [nguyenhai, dongbinh, tlybinh]
 
 export default accounts;
